9549f069735b87eea355168445c2feaf6c4f2270
leobvila <leobvila@hotmail.com>
2025-03-21 11:18:25 -0300

    acreentando switch

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
+// TENTEI FAZER UM FOR E UM WHILE PARA ex; EX IR SOMANDO  1000 AO XPHEROI
+//sozinho nao dei conta com ia (gemini) ate foi . porem para funcionar direito o nivel precisarai fazer função
+//PREFERI DEIXAR SO O QUE REALIZEI SEM AJUDA 
+
+
 let nomeHeroi = "Ogro da Montana";
-let xpHeroi = 5391
-
-
+let xpHeroi = 6391
+let objetoMaoDireita = "Arma lendaria"
 let nivel = ""
+
+
+
+switch (objetoMaoDireita) {
+    case "Espada":
+    case "Machado":
+    case "Arco":
+        xpHeroi += 500
+        break
+    case "Amuleto":
+    case "cajado":
+        xpHeroi += 300
+        break
+    case "maçã":
+    case "Pão":
+        xpHeroi += 100
+        break
+    case "Arma lendaria":
+        xpHeroi += 1000
+        break
+}
+
+
+
+
 
 if (xpHeroi >= 0 && xpHeroi < 1000) {
     nivel = "Ferro"
@@ -18,13 +47,14 @@ if (xpHeroi >= 0 && xpHeroi < 1000) {
     nivel = "Ascendente"
 } else if (xpHeroi > 9001 && xpHeroi <= 10000) {
     nivel = "Imortal"
-}else if(xpHeroi > 1000){
+} else if (xpHeroi > 1000) {
     nivel = "Radiante"
 } else {
     nivel = "Sem nivel."
 }
 
-console.log("O Herói de nome: " + nomeHeroi + " esta no nível: " + nivel )
 
+console.log("O Herói de nome: " + nomeHeroi + " esta no nível: " + nivel + " " + xpHeroi)
+ 
 
 
